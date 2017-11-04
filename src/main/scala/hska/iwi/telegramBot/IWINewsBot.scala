4b0fd6e6f5dd9530033457bab56e4fff8ab89e6c
Tobias Kerst <tobias.kerst@tobscore.com>
2017-11-04 22:46:15 +0100

    Stores settings information in Database
    These settings don't really do anything yet, but they are stored in the database to show that they work

--- src/main/scala/hska/iwi/telegramBot/IWINewsBot.scala
+++ src/main/scala/hska/iwi/telegramBot/IWINewsBot.scala
@@ -59,6 +59,9 @@ class IWINewsBot() extends TelegramBot with Polling with Commands {
         if (redis.sadd("users", user.id).getOrElse(0l).toInt == 1) {
           reply("You have successfully subscribed to the faculty news.")
           logger.info(s"User ${user} added to subscriptions.")
+
+          // Set configuration. Everything is set to true, since the user subscribes to all subjects by default.
+          redis.hmset(s"config:${user.id}", Map("INFB" -> true, "MKIB" -> true, "INFM" -> true))
         } else {
           reply("You already subscribed to the faculty news.")
           logger.info(s"${user} already subscribed")
@@ -86,13 +89,14 @@ class IWINewsBot() extends TelegramBot with Polling with Commands {
       try {
         if (redis.srem("users", user.id).getOrElse(0l).toInt == 1) {
           reply("You will not receive any notifications.")
+          logger.info(s"Deleting user data for $user")
+
+          // Remove the user data from the database
+          redis.del(s"user:${user.id}")
+          redis.del(s"config:${user.id}")
         } else {
           reply("You're currently not receiving any notifications.")
         }
-
-        logger.info(s"Deleting user data for $user")
-        // Remove the user data from the database
-        redis.del(s"user:${user.id}")
       }
       catch {
         case rte: RuntimeException => logger.error("Cannot connect to redis server"); logger.debug(rte.getMessage);
