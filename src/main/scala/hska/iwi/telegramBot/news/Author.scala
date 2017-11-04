75dd07fb6f36d550ffedaa0fb2847759d931c6c0
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2017-11-04 16:13:00 +0100

    Adds objects to contain entry information

---
+++ src/main/scala/hska/iwi/telegramBot/news/Author.scala
@@ -0,0 +1,5 @@
+package hska.iwi.telegramBot.news
+
+case class Author(name: String, email: String) {
+
+}
---
+++ src/main/scala/hska/iwi/telegramBot/news/Entry.scala
@@ -0,0 +1,5 @@
+package hska.iwi.telegramBot.news
+
+case class Entry(title: String, author: Author, id: String, updated: String, content: String, summary: String) {
+
+}
