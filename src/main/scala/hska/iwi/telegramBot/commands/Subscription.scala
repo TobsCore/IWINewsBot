c280ff9ec0c15636ff5511f69d23bb58c7f04c67
Tobias Kerst <tobias.kerst@tobscore.com>
2017-11-06 01:40:42 +0100

    Fixes bug where UserIDs would be stored in database
    The userId is an Int and not an object and eventhough the program tries to convert these numbers to object whenever possible, ints are stored in the db. This caused a NumberFormatException

--- src/main/scala/hska/iwi/telegramBot/commands/Subscription.scala
+++ src/main/scala/hska/iwi/telegramBot/commands/Subscription.scala
@@ -13,19 +13,19 @@ trait Subscription extends Commands with Instances with ObjectSerialization {
       {
         val userID = UserID(user.id)
         try {
-          if (redis.sadd("users", userID).getOrElse(0l).toInt == 1) {
+          if (redis.sadd("users", userID.id).getOrElse(0l).toInt == 1) {
             reply("You have successfully subscribed to the faculty news.")
             logger.info(s"User $user added to subscriptions.")
 
             // Set configuration. Everything is set to true, since the user subscribes to all subjects by default.
-            redis.hmset(s"config:$userID", Map("INFB" -> true, "MKIB" -> true, "INFM" -> true))
+            redis.hmset(s"config:${userID.id}", Map("INFB" -> true, "MKIB" -> true, "INFM" -> true))
           } else {
             reply("You already subscribed to the faculty news.")
             logger.info(s"$user already subscribed")
           }
 
           // Update the user data
-          redis.set(s"user:$userID", write(user))
+          redis.set(s"user:${userID.id}", write(user))
         } catch {
           case rte: RuntimeException =>
             logger.error("Cannot connect to redis server")
@@ -40,13 +40,13 @@ trait Subscription extends Commands with Instances with ObjectSerialization {
       {
         val userID = UserID(user.id)
         try {
-          if (redis.srem("users", userID).getOrElse(0l).toInt == 1) {
+          if (redis.srem("users", userID.id).getOrElse(0l).toInt == 1) {
             reply("You will not receive any notifications.")
             logger.info(s"Deleting user data for $user")
 
             // Remove the user data from the database
-            redis.del(s"user:$userID")
-            redis.del(s"config:$userID")
+            redis.del(s"user:${userID.id}")
+            redis.del(s"config:${userID.id}")
           } else {
             reply("You're currently not receiving any notifications.")
           }
