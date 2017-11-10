413629b5d49b2f5bf49e55f11701ed956879ab57
Tobias Kerst <tobias.kerst@tobscore.com>
2017-11-10 17:10:55 +0100

    Fixus buggy entryformatter
    Some titles couldn't be parsed correctly. This fixes the formatting of the entries.

--- src/main/scala/hska/iwi/telegramBot/news/EntryFormatter.scala
+++ src/main/scala/hska/iwi/telegramBot/news/EntryFormatter.scala
@@ -10,7 +10,8 @@ object EntryFormatter {
 
   def format(entry: Entry): String = {
     val title = entry.title.bold
-    val subTitle = entry.summary.replaceAll(": " + entry.content, "").bold
+    val splitSummary = entry.summary.split(": ", 2)
+    val subTitle = if (splitSummary.size >= 2) { "\n" + splitSummary(0).bold } else { "" }
     val summary = entry.content
     val authorName = entry.author.name.italic
     val authorEmail = entry.author.email
@@ -19,7 +20,6 @@ object EntryFormatter {
         .toString(DateTimeFormat.forPattern("d. MMM. yyyy - HH:mm 'Uhr'").withLocale(Locale.GERMAN))
 
     s"""|$title
-        |
         |$subTitle
         |$summary
         |
