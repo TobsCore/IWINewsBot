6f0ec724b5721ab0d0ee110a211b0fa0ae991283
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2017-11-29 02:54:09 +0100

    Adds mensa url to the FeedURLS

--- src/main/scala/hska/iwi/telegramBot/news/FeedURL.scala
+++ src/main/scala/hska/iwi/telegramBot/news/FeedURL.scala
@@ -2,4 +2,5 @@ package hska.iwi.telegramBot.news
 
 object FeedURL {
   val bulletinBoard = "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/newsbulletinboard"
+  val mensa = "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/canteen/v2/2/"
 }
