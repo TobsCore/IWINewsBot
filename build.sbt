TobsCore/IWINewsBot

commit 44d9dd0da2a8eb559ba05b41e2a3d40a52fd3af2

Moves callback Acknowledgement
ackcallback needs to be called anytime, so it is put at the beginning of the callback methods

src/main/scala/hska/iwi/telegramBot/commands/AboSettings.scala
@@ -54,6 +54,8 @@ trait AboSettings extends Commands with Callbacks with Instances {
   }
 
   onCallbackWithTag("Abo") { implicit cbq: CallbackQuery =>
+    callback(cbq)
+
     val tag = cbq.data
     if (tag.isDefined) {
       tag.get match {
@@ -87,7 +89,6 @@ trait AboSettings extends Commands with Callbacks with Instances {
           ackCallback(Some(notificationText4Faculty(setValue)))
         case _ => throw new IllegalArgumentException(s"Type ${course.getClass} is not allowed")
       }
-      callback(cbq)
     }
   }
 

src/main/scala/hska/iwi/telegramBot/commands/Lecturers.scala
@@ -24,7 +24,7 @@ trait Lecturers extends Commands with Callbacks {
     logger.debug(s"${msg.from.getOrElse("")} requested data about lecturers")
     val content = HTTPGet.get(FeedURL.lecturer)
     if (content.isDefined) {
-      //parses the json entries and stores them in a MensaMoltke object
+      //parses the json entries and stores them in a lecturers object
       lecturers = Some(JsonMethods.parse(content.get).extract[Seq[Lecturer]].sortBy(_.lastname))
       //reply(RoomFormatter.format(lecturers), parseMode = Some(ParseMode.HTML))
 
@@ -44,6 +44,8 @@ trait Lecturers extends Commands with Callbacks {
   }
 
   onCallbackWithTag("Lecturer") { implicit cbq: CallbackQuery =>
+    // Always needs to acknowledge the callback
+    ackCallback()(cbq)
     val lecturerID = cbq.data.get.toInt
     logger.info(s"Received lecturer with ID: $lecturerID")
 
@@ -53,7 +55,6 @@ trait Lecturers extends Commands with Callbacks {
     if (lecturers.isDefined) {
       val selectedLecturer = lecturers.get.find(lec => lec.id == lecturerID)
       if (selectedLecturer.isDefined) {
-        ackCallback()(cbq)
         request(
           EditMessageText(
             Some(chatId),

src/main/scala/hska/iwi/telegramBot/commands/Mensa.scala
@@ -67,6 +67,8 @@ trait Mensa extends Commands with Callbacks with Instances {
   }
 
   onCallbackWithTag("Mensa") { implicit cbq: CallbackQuery =>
+    ackCallback()(cbq)
+
     val mensaDayID = cbq.data.get.toInt
     val daysToAdd = setDaysToAddArray()
     val daysInFuture = daysToAdd(mensaDayID) + mensaDayID
@@ -81,7 +83,6 @@ trait Mensa extends Commands with Callbacks with Instances {
     val content = HTTPGet.get(mensaUrl)
 
     if (content.isDefined) {
-      ackCallback()(cbq)
       //parses the json entries and stores them in a MensaMoltke object
       val mensa = JsonMethods.parse(content.get).extract[MensaMoltke]
       val currentContent = cbq.message.get.text.getOrElse("")

commit 12fa326c8423a9c4a27e2b948a0b261d83f7bab1

Fixes callback issue with abo settings
Moving the callback in commit 44d9dd0da2a8eb559ba05b41e2a3d40a52fd3af2 caused an error, where the abo settings couldn't be changed. Avoiding this change will fix the regression.

build.sbt
@@ -1,5 +1,5 @@
 name := "IWINewsBot"
-version := "1.2.2"
+version := "1.2.3"
 scalaVersion := "2.12.4"
 
 

src/main/scala/hska/iwi/telegramBot/commands/AboSettings.scala
@@ -54,8 +54,6 @@ trait AboSettings extends Commands with Callbacks with Instances {
   }
 
   onCallbackWithTag("Abo") { implicit cbq: CallbackQuery =>
-    callback(cbq)
-
     val tag = cbq.data
     if (tag.isDefined) {
       tag.get match {
@@ -73,6 +71,7 @@ trait AboSettings extends Commands with Callbacks with Instances {
       logger.error(
         "There was no content to the callback method. This shouldn't happen, as the user cannot " +
           "set the subscriptions correctly. If this error occurs, check this.")
+      callback(cbq)
     } else {
 
       val userID: UserID = UserID(cbq.message.get.chat.id.toInt)
@@ -89,6 +88,7 @@ trait AboSettings extends Commands with Callbacks with Instances {
           ackCallback(Some(notificationText4Faculty(setValue)))
         case _ => throw new IllegalArgumentException(s"Type ${course.getClass} is not allowed")
       }
+      callback(cbq)
     }
   }
 