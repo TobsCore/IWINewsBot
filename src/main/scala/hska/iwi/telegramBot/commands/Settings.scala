fbb18f4388b86dda02eb234c4894ca305937b931
Tobias Kerst <tobias.kerst@tobscore.com>
2018-04-27 09:10:44 +0200

    Improves price settings to have several buttons

--- src/main/scala/hska/iwi/telegramBot/commands/Settings.scala
+++ src/main/scala/hska/iwi/telegramBot/commands/Settings.scala
@@ -16,10 +16,9 @@ trait Settings extends Commands with Callbacks with Instances {
       {
         logger.info(s"User $user selected /settings")
 
-        reply(
-          settingsGreeting,
-          replyMarkup = Some(createSettingsInlineKeyboardMarkup()),
-          parseMode = Some(ParseMode.HTML)
+        reply(settingsGreeting(UserID(user.id)),
+              replyMarkup = Some(createSettingsInlineKeyboardMarkup()),
+              parseMode = Some(ParseMode.HTML))
       }
     }
   }
@@ -31,24 +30,26 @@ trait Settings extends Commands with Callbacks with Instances {
   }
 
   def priceSettingInlineKeyBoardMarkup(userID: UserID): InlineKeyboardMarkup = {
-    val priceConfig = redis.getPriceConfigForUser(userID)
-    val config =
-      InlineKeyboardButton.callbackData(priceConfig.toString, prefixTag(priceTag)("0"))
-
-    val priceConfigButton = Seq[InlineKeyboardButton](config)
-    InlineKeyboardMarkup.singleColumn(priceConfigButton)
+    // Tuple consists of the printed name and the Tag name for each button
+    val buttons =
+      Seq(("Studierende", "Student"),
+          ("Mitarbeiter/innen", "Employee"),
+          ("Studierende und Mitarbeiter/innen", "Both"),
+          ("« Zurück zu Settings", "Back")).map(entry =>
+        InlineKeyboardButton.callbackData(text = entry._1, prefixTag(priceTag)(entry._2)))
+    InlineKeyboardMarkup.singleColumn(buttons)
   }
 
   onCallbackWithTag(settingsTag) { implicit cbq: CallbackQuery =>
     ackCallback()
     if (cbq.data.isDefined) {
       val response: (String, Option[InlineKeyboardMarkup]) = cbq.data.get match {
-        case "Abonnement" =>
+        case "Abonnement" => ("Not supported", None)
+        case "Preise" =>
           (priceSettingText(), Some(priceSettingInlineKeyBoardMarkup(UserID(cbq.from.id))))
-        case "Preise"  => ("Not supported", None)
         case "Studium" => ("Not supported", None)
         case topic =>
-          logger.error(s"Not supported tag has been used in general setting. The tag was: ${topic}")
+          logger.error(s"Not supported tag has been used in general setting. The tag was: $topic")
           ("Not supported tag", None)
 
       }
@@ -65,39 +66,75 @@ trait Settings extends Commands with Callbacks with Instances {
   }
 
   onCallbackWithTag(priceTag) { implicit cbq: CallbackQuery =>
-    val messageId = cbq.message.get.messageId
-    val chatId = cbq.message.get.chat.id
-    val userId = UserID(cbq.from.id)
-    val priceConfig = redis.getPriceConfigForUser(userId)
-    val newConfig = priceConfig.configValue match {
-      case "student"  => "employee"
-      case "employee" => "both"
-      case _          => "student"
+    val buttonData = cbq.data
+    val user = UserID(cbq.from.id)
+    if (buttonData.isDefined) {
+      val response: (String, Option[InlineKeyboardMarkup]) = buttonData.get match {
+        case "Student" =>
+          ackCallback(Some("Preise für Studierende wurde gespeichert"))
+          savePriceSettings(buttonData.get, user)
+          (settingsGreeting(user), Some(createSettingsInlineKeyboardMarkup()))
+        case "Employee" =>
+          ackCallback(Some("Preise für Mitarbeiter/innen wurde gespeichert"))
+          savePriceSettings(buttonData.get, user)
+          (settingsGreeting(user), Some(createSettingsInlineKeyboardMarkup()))
+        case "Both" =>
+          ackCallback(Some("Preise für Studierende und Mitarbeiter/innen wurde gespeichert"))
+          savePriceSettings(buttonData.get, user)
+          (settingsGreeting(user), Some(createSettingsInlineKeyboardMarkup()))
+        case "Back" =>
+          ackCallback()
+          (settingsGreeting(user), Some(createSettingsInlineKeyboardMarkup()))
+        case other =>
+          ackCallback()
+          logger.error(s"Not supported tag has been used in general setting. The tag was: $other")
+          ("Not supported tag", None)
+
+      }
+
+      request(
+        EditMessageText(
+          Some(cbq.message.get.chat.id),
+          Some(cbq.message.get.messageId),
+          replyMarkup = response._2,
+          text = response._1,
+          parseMode = Some(ParseMode.HTML)
+        ))
     }
-    redis.setPriceConfigForUser(PriceConfig(newConfig), userId)
-
-    val text = s"Preise für ${PriceConfig(newConfig).toString} sind ausgewählt"
-    ackCallback(Some(text))
-
-    request(
-      EditMessageText(
-        Some(chatId),
-        Some(messageId),
-        replyMarkup = Some(priceSettingInlineKeyBoardMarkup(userId)),
-        text = priceSettingText(),
-        parseMode = Some(ParseMode.HTML)
-      ))
   }
+
+  private def savePriceSettings(settings: String, userID: UserID): Unit = {
+    val priceConfig = PriceConfig(settings.toLowerCase())
+    redis.setPriceConfigForUser(priceConfig, userID)
+  }
+
   private def priceSettingText(): String =
     "Aus welcher Sicht möchtest Du die Mensapreise anzeigen lassen?"
 
-  private def settingsGreeting: String = {
-    // TODO: Fix this method to show actual information
+  private def settingsGreeting(userID: UserID): String = {
+    // Subscription information
+    val courseNewsInfo = redis.getConfigFor(userID) match {
+      case Some(aboMap) => aboMap.filter(_._2).keys.mkString(", ")
+      case None =>
+        logger.error(s"Could not retrieve config options for with id $userID")
+        ""
+    }
+    val facultyNewsSubscription = redis.getFacultyConfigForUser(userID)
+    val facultyInfo = if (facultyNewsSubscription.isDefined && facultyNewsSubscription.get) {
+      val seperator = if (!courseNewsInfo.isEmpty) ", " else ""
+      seperator + "IWI"
+    } else ""
+
+    val aboInfo =
+      if (courseNewsInfo.isEmpty && facultyInfo.isEmpty) "Keine" else courseNewsInfo + facultyInfo
+
+    // Price information
+    val priceSetting = redis.getPriceConfigForUser(userID).toString
+
     s"""<b>Einstellungen</b>
        |
-       |Abonniert: MKIB, INFB, IWI
-       |Preise: Studierende
-       |Studium: INFM (SoftwareEngineering), 2. Semester
+       |Abonniert: $aboInfo
+       |Preise: $priceSetting
        |
        |Du kannst die folgenden Dinge anpassen:
      """.stripMargin
