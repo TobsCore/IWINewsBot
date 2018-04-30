d4634bd8c8cda39832a9d4aa7496b9b9263f9fda
Tobias Kerst <tobias.kerst@tobscore.com>
2018-04-30 22:04:20 +0200

    Refactors settings

--- src/main/scala/hska/iwi/telegramBot/commands/Settings.scala
+++ src/main/scala/hska/iwi/telegramBot/commands/Settings.scala
@@ -1,7 +1,7 @@
 package hska.iwi.telegramBot.commands
 
 import hska.iwi.telegramBot.news._
-import hska.iwi.telegramBot.service._
+import hska.iwi.telegramBot.service.{Instances, PriceConfig, Tagging, UserID}
 import info.mukel.telegrambot4s.api.TelegramBot
 import info.mukel.telegrambot4s.api.declarative.{Callbacks, Commands}
 import info.mukel.telegrambot4s.methods.{EditMessageText, ParseMode}
@@ -9,37 +9,111 @@ import info.mukel.telegrambot4s.models._
 
 trait Settings extends Commands with Callbacks with Instances {
   _: TelegramBot =>
-  val settingsTag = "Settings"
-  val priceTag = "PriceConfig"
-  val aboTag = "AboConfig"
-
-  val MKIBSelectionTAG = "MKIB"
-  val INFBSelectionTAG = "INFB"
-  val INFMSelectionTAG = "INFM"
-  val FacultyNewsSelectionTAG = "Faculty"
 
   onCommand("/settings") { implicit msg =>
     using(_.from) { user =>
       {
         logger.info(s"User $user selected /settings")
 
-        reply(settingsGreeting(UserID(user.id)),
-              replyMarkup = Some(mainSettingsInlineKeyboardMarkup()),
+        reply(mainSettingsText(UserID(user.id)),
+              replyMarkup = Some(mainSettingMarkup()),
               parseMode = Some(ParseMode.HTML))
       }
     }
   }
 
-  def mainSettingsInlineKeyboardMarkup(): InlineKeyboardMarkup = {
-    val buttons = Seq("Abonnement", "Preise", "Studium").map(name =>
-      InlineKeyboardButton.callbackData(text = name, prefixTag(settingsTag)(name)))
+  onCallbackWithTag(Tagging.SETTINGS_PREFIX) { implicit cbq: CallbackQuery =>
+    ackCallback()
+    if (cbq.data.isDefined) {
+      val userID = UserID(cbq.from.id)
+      val response: (String, Option[InlineKeyboardMarkup]) = cbq.data.get match {
+        case Tagging.ABONNEMENT => (aboSettingsText(), aboSettingsMarkup(userID))
+        case Tagging.PRICES =>
+          (priceSettingsText(), Some(priceSettingsMarkup(userID)))
+        case Tagging.STUDIUM => ("Not supported", None)
+        case topic =>
+          logger.error(s"Not supported tag has been used in general setting. The tag was: $topic")
+          ("Not supported tag", None)
+
+      }
+
+      requestEditMessage(cbq.message.get.chat.id,
+                         cbq.message.get.messageId,
+                         response._1,
+                         response._2)
+    }
+  }
+
+  onCallbackWithTag(Tagging.PRICE_PREFIX) { implicit cbq: CallbackQuery =>
+    val buttonData = cbq.data
+    val user = UserID(cbq.from.id)
+    if (buttonData.isDefined) {
+      val response: (String, Option[InlineKeyboardMarkup]) = buttonData.get match {
+        case Tagging.STUDENT =>
+          ackCallback(Some("Preise für Studierende wurde gespeichert"))
+          savePriceSettings(buttonData.get, user)
+          (mainSettingsText(user), Some(mainSettingMarkup()))
+        case Tagging.EMPLOYEE =>
+          ackCallback(Some("Preise für Mitarbeiter/innen wurde gespeichert"))
+          savePriceSettings(buttonData.get, user)
+          (mainSettingsText(user), Some(mainSettingMarkup()))
+        case Tagging.BOTH =>
+          ackCallback(Some("Preise für Studierende und Mitarbeiter/innen wurde gespeichert"))
+          savePriceSettings(buttonData.get, user)
+          (mainSettingsText(user), Some(mainSettingMarkup()))
+        case Tagging.BACK =>
+          ackCallback()
+          (mainSettingsText(user), Some(mainSettingMarkup()))
+        case other =>
+          ackCallback()
+          logger.error(s"Not supported tag has been used in general setting. The tag was: $other")
+          ("Not supported tag", None)
+      }
+
+      requestEditMessage(cbq.message.get.chat.id,
+                         cbq.message.get.messageId,
+                         response._1,
+                         response._2)
+    }
+  }
+
+  onCallbackWithTag(Tagging.ABO_PREFIX) { implicit cbq: CallbackQuery =>
+    val buttonData = cbq.data
+    implicit val user: UserID = UserID(cbq.from.id)
+    if (buttonData.isDefined) {
+      logger.debug(s"$user is changing abo settings")
+      val msg = cbq.message.get
+      val response: (String, Option[InlineKeyboardMarkup]) = buttonData.get match {
+        case Tagging.MKIB        => saveAboSettings(MKIB)
+        case Tagging.INFB        => saveAboSettings(INFB)
+        case Tagging.INFM        => saveAboSettings(INFM)
+        case Tagging.FACULTYNEWS => saveAboSettings(Faculty)
+        case Tagging.BACK        => (mainSettingsText(user), Some(mainSettingMarkup()))
+        case _ =>
+          logger.error("Unknown tag received in callback for Abo settings.")
+          ("Es tut uns leid, aber es ist ein Fehler aufgetreten \uD83D\uDE48.", None)
+      }
+
+      requestEditMessage(msg.chat.id, msg.messageId, response._1, response._2)
+    }
+  }
+
+  def mainSettingMarkup(): InlineKeyboardMarkup = {
+    val buttons = createButtons(Seq(("Abonnement", Tagging.ABONNEMENT),
+                                    ("Preise", Tagging.PRICES),
+                                    ("Studium", Tagging.STUDIUM)),
+                                Tagging.SETTINGS_PREFIX)
     InlineKeyboardMarkup.singleColumn(buttons)
   }
 
-  def aboSettingsInlineKeyboardMarkup(user: UserID): Option[InlineKeyboardMarkup] = {
-
+  def aboSettingsMarkup(user: UserID): Option[InlineKeyboardMarkup] = {
     val configOption = redis.getConfigFor(user)
     val facultyNewsValueOption = redis.getFacultyConfigForUser(user)
+
+    // TODO: Should behave differently. If a user doesn't have any settings, yet, the default
+    // settings should be written to the database and then returned. This method should then not
+    // return an option and even better could be moved over to the createButtons method call,
+    // like all the others.
     if (configOption.isDefined && facultyNewsValueOption.isDefined) {
       val config = configOption.get
       val facultyNewsValue = facultyNewsValueOption.get
@@ -47,15 +121,16 @@ trait Settings extends Commands with Callbacks with Instances {
       Some(
         InlineKeyboardMarkup.singleColumn(
           Seq(
-            InlineKeyboardButton.callbackData(buttonText(!config(INFB), INFB),
-                                              prefixTag(aboTag)(INFBSelectionTAG)),
-            InlineKeyboardButton.callbackData(buttonText(!config(MKIB), MKIB),
-                                              prefixTag(aboTag)(MKIBSelectionTAG)),
-            InlineKeyboardButton.callbackData(buttonText(!config(INFM), INFM),
-                                              prefixTag(aboTag)(INFMSelectionTAG)),
+            InlineKeyboardButton.callbackData(aboButtonTexts(!config(INFB), INFB),
+                                              prefixTag(Tagging.ABO_PREFIX)(Tagging.INFB)),
+            InlineKeyboardButton.callbackData(aboButtonTexts(!config(MKIB), MKIB),
+                                              prefixTag(Tagging.ABO_PREFIX)(Tagging.MKIB)),
+            InlineKeyboardButton.callbackData(aboButtonTexts(!config(INFM), INFM),
+                                              prefixTag(Tagging.ABO_PREFIX)(Tagging.INFM)),
             InlineKeyboardButton.callbackData(buttonText4Faculty(!facultyNewsValue),
-                                              prefixTag(aboTag)(FacultyNewsSelectionTAG)),
-            InlineKeyboardButton.callbackData("« Zurück zu Settings", prefixTag(aboTag)("Back"))
+                                              prefixTag(Tagging.ABO_PREFIX)(Tagging.FACULTYNEWS)),
+            InlineKeyboardButton.callbackData("« Zurück zu Settings",
+                                              prefixTag(Tagging.ABO_PREFIX)("Back"))
           )
         ))
     } else {
@@ -63,142 +138,67 @@ trait Settings extends Commands with Callbacks with Instances {
     }
   }
 
-  def priceSettingsInlineKeyboardMarkup(userID: UserID): InlineKeyboardMarkup = {
-    // Tuple consists of the printed name and the Tag name for each button
+  def priceSettingsMarkup(userID: UserID): InlineKeyboardMarkup = {
     val buttons =
-      Seq(("Studierende", "Student"),
-          ("Mitarbeiter/innen", "Employee"),
-          ("Studierende und Mitarbeiter/innen", "Both"),
-          ("« Zurück zu Settings", "Back")).map(entry =>
-        InlineKeyboardButton.callbackData(text = entry._1, prefixTag(priceTag)(entry._2)))
+      createButtons(
+        Seq(
+          ("Studierende", Tagging.STUDENT),
+          ("Mitarbeiter/innen", Tagging.EMPLOYEE),
+          ("Studierende und Mitarbeiter/innen", Tagging.BOTH),
+          ("« Zurück zu Settings", Tagging.BACK)
+        ),
+        Tagging.PRICE_PREFIX
+      )
     InlineKeyboardMarkup.singleColumn(buttons)
   }
 
-  onCallbackWithTag(settingsTag) { implicit cbq: CallbackQuery =>
-    ackCallback()
-    if (cbq.data.isDefined) {
-      val userID = UserID(cbq.from.id)
-      val response: (String, Option[InlineKeyboardMarkup]) = cbq.data.get match {
-        case "Abonnement" => (aboSettingsText(), aboSettingsInlineKeyboardMarkup(userID))
-        case "Preise" =>
-          (priceSettingText(), Some(priceSettingsInlineKeyboardMarkup(userID)))
-        case "Studium" => ("Not supported", None)
-        case topic =>
-          logger.error(s"Not supported tag has been used in general setting. The tag was: $topic")
-          ("Not supported tag", None)
-
-      }
-
-      request(
-        EditMessageText(
-          Some(cbq.message.get.chat.id),
-          Some(cbq.message.get.messageId),
-          replyMarkup = response._2,
-          text = response._1,
-          parseMode = Some(ParseMode.HTML)
-        ))
-    }
+  def studiumCourseSettingsMarkup(userID: UserID): InlineKeyboardMarkup = {
+    val buttons =
+      createButtons(Seq(("INFB", Tagging.INFB),
+                        ("MKIB", Tagging.MKIB),
+                        ("INFM", Tagging.INFM),
+                        ("« Zurück zu Settings", Tagging.BACK)),
+                    Tagging.STUDIUM_PREFIX)
+    InlineKeyboardMarkup.singleColumn(buttons)
   }
 
-  onCallbackWithTag(priceTag) { implicit cbq: CallbackQuery =>
-    val buttonData = cbq.data
-    val user = UserID(cbq.from.id)
-    if (buttonData.isDefined) {
-      val response: (String, Option[InlineKeyboardMarkup]) = buttonData.get match {
-        case "Student" =>
-          ackCallback(Some("Preise für Studierende wurde gespeichert"))
-          savePriceSettings(buttonData.get, user)
-          (settingsGreeting(user), Some(mainSettingsInlineKeyboardMarkup()))
-        case "Employee" =>
-          ackCallback(Some("Preise für Mitarbeiter/innen wurde gespeichert"))
-          savePriceSettings(buttonData.get, user)
-          (settingsGreeting(user), Some(mainSettingsInlineKeyboardMarkup()))
-        case "Both" =>
-          ackCallback(Some("Preise für Studierende und Mitarbeiter/innen wurde gespeichert"))
-          savePriceSettings(buttonData.get, user)
-          (settingsGreeting(user), Some(mainSettingsInlineKeyboardMarkup()))
-        case "Back" =>
-          ackCallback()
-          (settingsGreeting(user), Some(mainSettingsInlineKeyboardMarkup()))
-        case other =>
-          ackCallback()
-          logger.error(s"Not supported tag has been used in general setting. The tag was: $other")
-          ("Not supported tag", None)
-      }
-
-      request(
-        EditMessageText(
-          Some(cbq.message.get.chat.id),
-          Some(cbq.message.get.messageId),
-          replyMarkup = response._2,
-          text = response._1,
-          parseMode = Some(ParseMode.HTML)
-        ))
-    }
+  def studiumSpecialisationSettingsMarkup(userID: UserID): InlineKeyboardMarkup = {
+    // Tuple consists of the printed name and the Tag name for each button
+    val buttons =
+      createButtons(
+        Seq(
+          ("Software-Engineering", Tagging.SOFTWARE_ENGINEERING),
+          ("Interaktive Systeme", Tagging.INTERACTIVE_SYSTEMS),
+          ("« Zurück zu Settings", Tagging.BACK)
+        ),
+        Tagging.SPECIALILISATION_PREFIX
+      )
+    InlineKeyboardMarkup.singleColumn(buttons)
   }
 
-  onCallbackWithTag(aboTag) { implicit cbq: CallbackQuery =>
-    val buttonData = cbq.data
-    val user = UserID(cbq.from.id)
-    if (buttonData.isDefined) {
-      val response: (String, Option[InlineKeyboardMarkup]) = buttonData.get match {
-        case MKIBSelectionTAG        => callbackMethod(MKIB)
-        case INFBSelectionTAG        => callbackMethod(INFB)
-        case INFMSelectionTAG        => callbackMethod(INFM)
-        case FacultyNewsSelectionTAG => callbackMethod(Faculty)
-        case "Back" =>
-          (settingsGreeting(user), Some(mainSettingsInlineKeyboardMarkup()))
-
-        case _ => throw new IllegalArgumentException
-      }
-
-      request(
-        EditMessageText(
-          Some(cbq.message.get.chat.id),
-          Some(cbq.message.get.messageId),
-          replyMarkup = response._2,
-          text = response._1,
-          parseMode = Some(ParseMode.HTML)
-        ))
-    }
+  def createButtons(seq: Seq[(String, String)], tagPrefix: String): Seq[InlineKeyboardButton] = {
+    seq.map(entry =>
+      InlineKeyboardButton.callbackData(text = entry._1, prefixTag(tagPrefix)(entry._2)))
   }
 
-  private def callbackMethod(course: SubscribableMember)(
-      implicit cbq: CallbackQuery): (String, Option[InlineKeyboardMarkup]) = {
-    if (cbq.message.isEmpty) {
-      logger.error(
-        "There was no content to the callback method. This shouldn't happen, as the user cannot " +
-          "set the subscriptions correctly. If this error occurs, check this.")
-      aboSettingsCallback(cbq)
-    } else {
+  private def saveAboSettings(course: SubscribableMember)(
+      implicit user: UserID,
+      cbq: CallbackQuery): (String, Option[InlineKeyboardMarkup]) = {
 
-      val userID: UserID = UserID(cbq.message.get.chat.id.toInt)
-      course match {
-        case course: Course =>
-          val setValue = !redis.getConfigFor(userID).getOrElse(Map()).find(_._1 == course).head._2
-          redis.setUserConfig(userID, course, setValue)
-          // Notification only shown to the user who pressed the button.
-          ackCallback(Some(notificationText(setValue, course)))
-        case _: Faculty.type =>
-          val setValue = !redis.getFacultyConfigForUser(userID).getOrElse(false)
-          redis.setFacultyConfigForUser(userID, setValue)
-          // Notification only shown to the user who pressed the button.
-          ackCallback(Some(notificationText4Faculty(setValue)))
-        case _ => throw new IllegalArgumentException(s"Type ${course.getClass} is not allowed")
-      }
-      aboSettingsCallback(cbq)
+    course match {
+      case course: Course =>
+        val setValue = !redis.getConfigFor(user).getOrElse(Map()).find(_._1 == course).head._2
+        redis.setUserConfig(user, course, setValue)
+        ackCallback(Some(notificationText(setValue, course)))
+      case _: Faculty.type =>
+        val setValue = !redis.getFacultyConfigForUser(user).getOrElse(false)
+        redis.setFacultyConfigForUser(user, setValue)
+        ackCallback(Some(notificationText4Faculty(setValue)))
+      case _ =>
+        logger.error(s"Type ${course.getClass} is not allowed")
+        ackCallback()
     }
-  }
-
-  private def aboSettingsCallback(cbq: CallbackQuery): (String, Option[InlineKeyboardMarkup]) = {
-    if (cbq.message.isDefined) {
-      val user = UserID(cbq.message.get.chat.id.toInt)
-      (aboSettingsText(), aboSettingsInlineKeyboardMarkup(user))
-    } else {
-      logger.error(
-        "No message has been defined for selection of abo settings. This shouldn't happen.")
-      ("Ein Fehler ist aufgetreten", None)
-    }
+    (aboSettingsText(), aboSettingsMarkup(user))
   }
 
   private def savePriceSettings(settings: String, userID: UserID): Unit = {
@@ -211,10 +211,10 @@ trait Settings extends Commands with Callbacks with Instances {
       s" <i>Schwarzen Bretts</i> erhalten möchtest. Außerdem kannst Du Nachrichten der " +
       s"IWI-Fakultät abonnieren."
 
-  private def priceSettingText(): String =
+  private def priceSettingsText(): String =
     "Aus welcher Sicht möchtest Du die Mensapreise anzeigen lassen?"
 
-  private def settingsGreeting(userID: UserID): String = {
+  private def mainSettingsText(userID: UserID): String = {
     // Subscription information
     val courseNewsInfo = redis.getConfigFor(userID) match {
       case Some(aboMap) => aboMap.filter(_._2).keys.mkString(", ")
@@ -233,7 +233,6 @@ trait Settings extends Commands with Callbacks with Instances {
 
     // Price information
     val priceSetting = redis.getPriceConfigForUser(userID).toString
-
     s"""<b>Einstellungen</b>
        |
        |Abonniert: $aboInfo
@@ -243,32 +242,46 @@ trait Settings extends Commands with Callbacks with Instances {
      """.stripMargin
   }
 
-  private def buttonText(value: Boolean, course: SubscribableMember): String =
+  private def aboButtonTexts(value: Boolean, course: SubscribableMember): String =
     if (value) {
       s"$course abonnieren"
     } else {
       s"$course abbestellen"
     }
 
-  def buttonText4Faculty(value: Boolean): String =
+  private def buttonText4Faculty(value: Boolean): String =
     if (value) {
       s"IWI-Fakultät abonnieren"
     } else {
       s"IWI-Fakultät abbestellen"
     }
 
-  def notificationText4Faculty(selection: Boolean): String =
+  private def notificationText4Faculty(selection: Boolean): String =
     if (selection) {
       s"Nachrichten der Fakultät sind abonniert"
     } else {
       s"Nachrichten der Fakultät sind abbestellt"
     }
 
-  def notificationText(selection: Boolean, course: Course): String =
+  private def notificationText(selection: Boolean, course: Course): String =
     if (selection) {
       s"Nachrichten für $course sind abonniert"
     } else {
       s"Nachrichten für $course sind abbestellt"
     }
+
+  private def requestEditMessage(chatID: ChatId,
+                                 messageID: Int,
+                                 text: String,
+                                 markup: Option[InlineKeyboardMarkup]): Unit = {
+    request(
+      EditMessageText(
+        Some(chatID),
+        Some(messageID),
+        replyMarkup = markup,
+        text = text,
+        parseMode = Some(ParseMode.HTML)
+      ))
+  }
 
 }
