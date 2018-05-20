96f91fa8631d970e1458ab201a37edf3278cb943
Tobias Kerst <tobias.kerst@tobscore.com>
2018-05-20 23:52:42 +0200

    Loads .rive files from withing resources/
    The rive files are now placed in the resources/ folder as to store them in a generated jar and have them included in a runnable executable.

--- src/main/scala/hska/iwi/telegramBot/ChatBot/ChatBot.scala
+++ src/main/scala/hska/iwi/telegramBot/ChatBot/ChatBot.scala
@@ -1,9 +1,26 @@
 package hska.iwi.telegramBot.ChatBot
 
 import com.rivescript.{Config, RiveScript}
+import com.typesafe.scalalogging.Logger
+
+import scala.io.Source
 
 class ChatBot extends RiveScript(Config.utf8()) {
-  loadFile("./telegramChatBotMain.rive")
+  val logger = Logger(getClass)
+
+  /**
+    * The rive files need to be read from the resources folder, since it is included in the jar
+    * file and these files are needed for execution. In a previous version, these files were
+    * stored in the projects root directory but wouldn't be exported. Reading those files should
+    * be done by reading the input stream, since loading the files failed. See https://github
+    * .com/typesafehub/activator-akka-stream-scala/issues/39 for a comment regarding the read
+    * method.
+    */
+  private val folder = "chatbot"
+  private val mainStream = getClass.getClassLoader.getResourceAsStream(s"$folder/main.rive")
+  private val synonyms = getClass.getClassLoader.getResourceAsStream(s"$folder/synonyms.rive")
+  stream(Source.fromInputStream(mainStream).getLines.toArray)
+  stream(Source.fromInputStream(synonyms).getLines.toArray)
   sortReplies()
 
   // Add Subroutines here
